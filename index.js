c3904a959a2cc192771c22a2e7b8e3cb1616fbba
UnluckyFroggy <50998029+UnluckyFroggy@users.noreply.github.com>
2021-09-02 11:57:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const mongoose = require('./data/mongo.js')()
 
 const mongoExample = require('./data/models/example.js')
 
-io.on('connection', socket => {
-  console.log('Sockets Connected!')
-})
-
 const port = '8080';
 
 server.listen(port, function() {
